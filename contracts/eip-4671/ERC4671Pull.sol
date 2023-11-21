453068c972554c474145141132b7d7492bbe1f9c
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-11-21 11:00:56 +0100

    upgrade to solidity 0.8.18

--- contracts/eip-4671/ERC4671Pull.sol
+++ contracts/eip-4671/ERC4671Pull.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
